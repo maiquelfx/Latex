86ada0d385aee43d4acb086bbb9499cafd772578
maiquelfx <maiquelfx@gmail.com>
2026-04-29 00:27:11 -0300

    equacao 2026-04-29_00-27-11

--- IC-UFF/0 OTHERS/github.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set msg=inicio das verificacoes
-for /f %%i in ('powershell -Command "Get-Date -Format yyyy-MM-dd_HH-mm-ss"') do set dt=%%i
-
-C:\msys64\usr\bin\bash.exe -lc "cd /home/Win/git/latex/latex/IC-UFF && git add . && git commit -m '%msg% %dt%' && git push"
-
-pause
